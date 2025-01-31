421b498728e072e2e9e2dd9419284b2c7a35a586
mubarratc <mubarratc@mercy.ngo>
2025-01-31 09:42:41 -0600

    adding some better styling and javascript

---
+++ dhikrCounter.js
@@ -0,0 +1,9 @@
+let counterText = document.getElementById("counter");
+console.log(counterText);
+
+function increase() {
+  let counter = 0;
+  counter = counter + 1;
+  counterText.textContent(counter);
+  counter.log(counter);
+}
