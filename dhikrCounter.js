eb6d6b765151717be03c66e47977de536685f9c9
mubarratc <mubarratc@mercy.ngo>
2025-02-16 07:12:02 -0600

    linked new writing page

--- dhikrCounter.js
+++ dhikrCounter.js
@@ -19,5 +19,5 @@ window.addEventListener("load", function () {
 // 1) Create a visitor counter that logs the amount of personal visits ✅
 // 2) Figure out a better color scheme and provide enough margin in the desktop version of this website.
 // 3) make the website mobile compatible.
-// 4) Successfully link to a different page that host a list of all of my essays
+// 4) Successfully link to a different page that host a list of all of my essays ✅
 // 5) Create a CMS system for all of my essays
